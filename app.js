a37d293f772a8786eff288f5de25c61c968970af
VladTGeorgiev <vlad.georgiev@outlook.com>
2019-10-22 21:54:31 +0100

    Added darksky.net API url

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('Starting')
+const request = require('request');
+
+const url = 'https://api.darksky.net/forecast/a33639af39e09017cf8de46ad4b4427f/37.8267,-122.4233?units=si';
+
+request({ url: url, json: true }, (error, response) => {
+    console.log(`${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degrees out. There is ${response.body.currently.precipProbability}% chance of rain.`)
+});
