f720b1fd01a9fed97fb5bc3345c54ada176017dd
VladTGeorgiev <vlad.georgiev@outlook.com>
2019-10-22 22:16:13 +0100

    Added mapbox.com API for geocoding

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const request = require('request');
 const url = 'https://api.darksky.net/forecast/a33639af39e09017cf8de46ad4b4427f/37.8267,-122.4233?units=si';
 
 const geocode =
-	'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoidmxhZHRnZW9yZ2lldiIsImEiOiJjazIyYno5ZWIxbjl0M2VxdGVxeGZxaGJsIn0.D-MQllZhGMdiTA15qKMjgw&limit=1';
+	'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=API_KEY';
 
 request({ url: url, json: true }, (error, response) => {
 	console.log(
